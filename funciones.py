cf67a640eb9366a96be20b0d0927e4f97d3bb39a
ClaudioDev <aleledesma687@gmail.com>
2025-03-28 23:12:36 -0300

    agregado boton de salida y editar

--- funciones.py
+++ funciones.py
@@ -5,9 +5,9 @@ def get_todos(filepath=FILEPATH):
     """ Leer in archivo de texto y devolver la lista de
     tareas pendientes.
     """
-    with open(filepath,"r") as file:
-            todos = file.readlines()
-    return todos
+    with open(filepath,"r") as file_local:
+            todos_local = file_local.readlines()
+    return todos_local
  
  
 def write_todos(todos_arg , filepath=FILEPATH):
